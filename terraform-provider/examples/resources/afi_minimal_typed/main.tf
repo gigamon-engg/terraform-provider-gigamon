6bdfd7af65485205f270f1b2c70bafbe6b035b81
Mustaq Mohamed Ali <mustaq.mohamedali@gigamon.com>
2026-07-24 06:56:52 +0000

    FOM-201 App rules support added

--- terraform-provider/examples/resources/afi_minimal_typed/main.tf
+++ terraform-provider/examples/resources/afi_minimal_typed/main.tf
@@ -19,40 +19,17 @@ provider "gigamon" {
 locals {
   monitoring_domain_id = "monitoringDomin::vmware::83ef84c6-4889-4594-b8f8-6824cf90dea7"
   connection_id = "connection::vmware::e8ae7cf3-1563-46f6-a490-1a6362e4a3a9"
-  monitoring_session_id = "monitoringSession::vmware::3c1bde50-c3d5-403f-8576-d7f60f5c5657"
+  monitoring_session_id = "monitoringSession::vmware::2e100e70-cf05-43e7-9278-d061eb004ab0"
 }
 
 
-
-
-resource "gigamon_monitoring_session" "uctv_ms" {
-  alias                = "tf-uctv-ms-1"
-  description          = "UCTV MS for 3PO drift tests"
-  monitoring_domain_id = local.monitoring_domain_id
-  connection_id        = local.connection_id
-
-  tapping_method = "uctv"
-
-  fast_mode          = false
-  scale_unit         = 1
-  distribute_traffic = true
-
-  traffic_acquisition = {
-    mirroring = {
-      secure_tunnels_enabled = false
-    }
-    precryption = {
-      secure_tunnels_enabled = false
-    }
-  }
-}
 
 ########################################
 # AFI Traffic Map for MS1
 ########################################
 resource "gigamon_traffic_map" "tm_afi" {
   name                  = "tm-uctv-afi"
-  monitoring_session_id = gigamon_monitoring_session.uctv_ms.id
+  monitoring_session_id = local.monitoring_session_id
   description           = "AFI validation traffic map"
 
   asf = {
@@ -117,131 +94,35 @@ resource "gigamon_traffic_map" "tm_afi" {
           }
         }
       ]
-    }
-  ]
-}
-
-
-resource "gigamon_app_ami" "ami_main" {
-  alias                 = "tf-ami-main"
-  monitoring_session_id = gigamon_monitoring_session.uctv_ms.id
-  description           = "AMI app validation with nested ipv6 next_header bug repro"
-
-  app_metadata = {
-    flow_behavior    = "bidir"
-    multi_collect    = true
-    aggregate_mode   = false
-    observ_domain_id = 0
-    dpi_inject_limit = 30
-
-    timeout = {
-      idle = 299
-    }
-
-    match = {
-      ipv4 = {
-        destination = {
-          prefix_min_mask = "32"
-        }
-       
-        source = {
-          prefix_min_mask = "24"
-        }
-      }
-    }
-
-    exporters = [
-      {
-        aep_id = 5
-        name   = "ami-exporter-1"
-
-        exporter_config = {
-          type         = "cef"
-          max_pkt_size = 0
-
-          app_profile_config = [
-            {
-              application_id = true
-              family_id      = false
-              tag_id         = true
-              type           = "export"
-
+      app_rules = {
+        pass_rules = [
+          {
+        
+            app_profile_config = {
               applications = [
-                {
-                  name = "app-1"
-                  attributes = [
-                    {
-                      name  = "attr1"
-                      value = "value1"
-                    }
-                  ]
-                }
+               {name= "360-safeguard"}, 
+               {name= "eset"}
               ]
-
-              counter = {
-                bytes           = false
-                bytes_long      = true
-                packets         = false
-                packets_long    = true
-                inner_byte      = false
-                inner_byte_long = true
-              }
+              type = "filter"
             }
-          ]
-
-          cef = {
-            active_timeout   = 60
-            inactive_timeout = 15
-            record_type      = "segregated"
           }
-        }
+        ]
+        drop_rules = [
+          {
+    
+            app_profile_config = {
+              applications = [
+               {name = "lookout-ms"}, 
+               {name = "sophos-update"}
+              ]
+              type = "filter"
+            }
+          }
+        ]
       }
-    ]
-  }
-}
-
-
-
-
-
-resource "gigamon_tunnel_out" "udp_out" {
-  monitoring_session_id = gigamon_monitoring_session.uctv_ms.id
-  alias                 = "ami-udp-out"
-
-  description = "UDP egress tunnel for AMI"
-  remote_ip   = "198.51.100.10"
-
-  udp {
-    source_port      = 50000
-    destination_port = 50001
-  }
-}
-
-
-########################################
-# Links for AFI -> AMI -> TEP
-########################################
-
-resource "gigamon_link" "afi_to_ami" {
-  monitoring_session_id = gigamon_monitoring_session.uctv_ms.id
-
-  source_id     = gigamon_traffic_map.tm_afi.id
-  source_aep_id = 4
-  dest_id       = gigamon_app_ami.ami_main.id
-
-  depends_on = [
-    gigamon_traffic_map.tm_afi,
-    gigamon_app_ami.ami_main,
+    }
   ]
 }
-
-resource "gigamon_link" "ami_to_udp" {
-  monitoring_session_id = gigamon_monitoring_session.uctv_ms.id
-  source_aep_id         = 5
-  source_id             = gigamon_app_ami.ami_main.id
-  dest_id               = gigamon_tunnel_out.udp_out.id
-}
-
 
 
 
--- terraform-provider/internal/commonresources/maps.go
+++ terraform-provider/internal/commonresources/maps.go
@@ -90,6 +90,7 @@ func (tm *TrafficMap) ValidateConfig(
 	}
 
 	applyAndValidateAsfSessionFields(cfg.Asf, &resp.Diagnostics)
+	validateAppRulesRequireAsf(&cfg, &resp.Diagnostics)
 }
 
 // Initial Configure call, to initialize the Provider
@@ -130,6 +131,10 @@ func (tm *TrafficMap) Create(ctx context.Context, req resource.CreateRequest, re
 		return
 	}
 	applyAndValidateAsfSessionFields(data.Asf, &resp.Diagnostics)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	validateAppRulesRequireAsf(&data, &resp.Diagnostics)
 	if resp.Diagnostics.HasError() {
 		return
 	}
@@ -235,6 +240,10 @@ func (tm *TrafficMap) Update(ctx context.Context, req resource.UpdateRequest, re
 		return
 	}
 	applyAndValidateAsfSessionFields(planData.Asf, &resp.Diagnostics)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	validateAppRulesRequireAsf(&planData, &resp.Diagnostics)
 	if resp.Diagnostics.HasError() {
 		return
 	}
@@ -572,6 +581,32 @@ func validateNilPassRules(ruleSets []RuleSetModel, diags *diag.Diagnostics) {
 					"Remove pass_rules from this rule_set.",
 			)
 		}
+	}
+}
+
+func validateAppRulesRequireAsf(data *MapModel, diags *diag.Diagnostics) {
+	if data == nil {
+		return
+	}
+
+	hasAsf := data.Asf != nil && data.Asf.AsfProfileConfig != nil
+	if hasAsf {
+		return
+	}
+
+	for i, rs := range data.RuleSets {
+		if rs.AppRules == nil {
+			continue
+		}
+		if len(rs.AppRules.PassRules) == 0 && len(rs.AppRules.DropRules) == 0 {
+			continue
+		}
+
+		diags.AddAttributeError(
+			path.Root("rule_sets").AtListIndex(i).AtName("app_rules"),
+			"app_rules require ASF",
+			"rule_sets.app_rules is supported only when asf.asf_profile_config is configured on the traffic map.",
+		)
 	}
 }
 
